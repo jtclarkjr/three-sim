1a31a13323cd2c7dbcfb9b675104b652a7271b9a
J <jtclarkjr@ucla.edu>
2025-11-12 23:23:14 +0900

    fix: shadow position

--- src/components/_resuables/Floor.tsx
+++ src/components/_resuables/Floor.tsx
@@ -1,6 +1,6 @@
 export const Floor = () => {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.5, 0]} receiveShadow>
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.8, 0]} receiveShadow>
       <planeGeometry args={[10, 10]} />
       <shadowMaterial opacity={0.3} />
     </mesh>
